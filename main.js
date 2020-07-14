286edd1fea08fae6ab13cefb139143b621df9505
Jean <jean.soro007@gmail.com>
2020-07-14 18:33:17 -0400

    Completed styles for header, header menu and added toggle functionality

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-//Select element function
+// hamburger menu trigger function
+
+const selectHamburger = (toggleMenuElement) => {
+  return document.querySelector(toggleMenuElement);
+}
+
+
+let menuToggler = selectHamburger('.menu-toggle');
+let body = selectHamburger('body');
+
+
+menuToggler.addEventListener('click', () => {
+  body.classList.toggle('open');
+})
